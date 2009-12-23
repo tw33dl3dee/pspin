d3d8ca36b1da80e9b41d567bc61a9b58803e2166
Ivan Korotkov <twee@tweedle-dee.org>
2009-12-23 21:10:58 +0300

    # Expression code generation added (without channel operations, send/receive and @run@ for now), closes #3. # Moved @VarRef@ to @Expr@ hiearchy.

--- src/pmlparse/expression.py
+++ src/pmlparse/expression.py
@@ -6,17 +6,97 @@ class Expr(object):
         return self.code()
 
 
-class RunExpr(Expr):
+class ConstExpr(Expr):
+    """Constant expression
     """
+    
+    def __init__(self, value):
+        """
+        
+        Arguments:
+        - `value`: Expression value (int)
+        """
+        Expr.__init__(self)
+        self._value = value
+
+    def code(self):
+        return str(self._value)
+
+
+class VarRef(Expr):
+    """Reference to a variable
+    """
+    pass
+
+
+class SimpleRef(VarRef):
+    """Direct reference to a variable
+    """
+    
+    def __init__(self, var):
+        """
+        
+        Arguments:
+        - `var`: Variable object
+        """
+        Expr.__init__(self)
+        self._var = var
+
+    def code(self):
+        return self._var.ref()
+
+
+class IdxRef(VarRef):
+    """Indexed reference to a variable
+    """
+    
+    def __init__(self, var, idx):
+        """
+        
+        Arguments:
+        - `var`: Variable object
+        - `idx`: Index (int)
+        """
+        Expr.__init__(self)
+        self._var = var
+        self._idx = idx
+
+    def code(self):
+        return "(%s[%s])" % (self._var.ref(), self._idx)
+
+
+class FieldRef(VarRef):
+    """Reference to a field of another variable reference
+    """
+    
+    def __init__(self, varref, field):
+        """
+        
+        Arguments:
+        - `varname`: VarRef object
+        - `field`:Field name
+        """
+        Expr.__init__(self)
+        self._varref = varref
+        self._field = field
+        
+    def ref(self):
+        return "(%s.%s)" % (self._varref.code(), self._idx)
+
+
+class RunExpr(Expr):
+    """Run process
     """
     
     def __init__(self, proctype, args, prio):
         """
         
         Arguments:
-        - `proctype`:
-        - `args`:
+        - `proctype`: Process name
+        - `args`: Arguments (list of Expr objects)
+        - `prio`: Priority (int)
         """
+        Expr.__init__(self)
         self._proctype = proctype
         self._args = args
         self._prio = prio
@@ -24,23 +104,24 @@ class RunExpr(Expr):
     def code(self):
         """
         """
-        return "<run (%s, %s, %s)>" % (self._proctype, self._args, self._prio)
+        return NotImplemented
 
 
 class TernaryExpr(Expr):
-    """
+    """Ternary expression (the only instance of which in PROMELA is ?:)
     """
     
     def __init__(self, left, op1, mid, op2, right):
         """
         
         Arguments:
-        - `left`:
-        - `op1`:
-        - `mid`:
-        - `op2`:
-        - `right`:
+        - `left`: Left Expr object
+        - `op1`: First part of op (str)
+        - `mid`: Mid Expr object
+        - `op2`: Second part of op (str)
+        - `right`: Right Expr object
         """
+        Expr.__init__(self)
         self._left = left
         self._op1 = op1
         self._mid = mid
@@ -50,67 +131,65 @@ class TernaryExpr(Expr):
     def code(self):
         """
         """
-        return "(%s %s %s %s %s)" % (self._left, self._op1, self._mid, self._op2, self._right)
+        return "(%s %s %s %s %s)" % (self._left.code(), self._op1, self._mid.code(), self._op2, self._right.code())
 
 
 class BinaryExpr(Expr):
-    """
+    """Binary expression
     """
     
     def __init__(self, left, op, right):
         """
         
         Arguments:
-        - `left`:
-        - `op`:
-        - `right`:
+        - `left`: Left Expr object
+        - `op`: Operator (str)
+        - `right`: Right Expr object
         """
+        Expr.__init__(self)
         self._left = left
         self._op = op
         self._right = right
 
     def code(self):
-        return "(%s %s %s)" % (self._left, self._op, self._right)
+        return "(%s %s %s)" % (self._left.code(), self._op, self._right.code())
 
 
 class UnaryExpr(Expr):
-    """
+    """Unary expression
     """
     
     def __init__(self, op, right):
         """
         
         Arguments:
-        - `op`:
-        - `right`:
+        - `op`: Unary operator
+        - `right`: Operand Expr object
         """
+        Expr.__init__(self)
         self._op = op
         self._right = right
 
     def code(self):
-        return "(%s %s)" % (self._op, self._right)
-
-
-class FuncallExpr(object):
-    def code(self):
-        return "<funcall"
+        return "(%s %s)" % (self._op, self._right.code())
 
 
 class ChanOpExpr(Expr):
-    """
+    """Channel operation (len, full, etc)
     """
     
     def __init__(self, op, chan):
         """
         
         Arguments:
-        - `op`:
-        - `chan`:
+        - `op`: Operator name (str)
+        - `chan`: Channel (VarRef object)
         """
+        Expr.__init__(self)
         self._op = op
         self._chan = chan
     
     def code(self):
         """
         """
-        return "chan_%s(%s)" % (self._op, self._chan)
+        return "chan_%s(%s)" % (self._op, self._chan.code())
