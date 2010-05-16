c70831c8e7f6474f3bb2296bfa1f724b6eb8dd8e
Ivan Korotkov <twee@tweedle-dee.org>
2010-05-16 15:07:05 +0400

    Added script to run SPIN with bit-state hashing.

---
+++ src/pspin/bspin-test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+MODEL=test.pml
+
+# FULLSTATE
+#spin -a $MODEL && gcc -DNOREDUCE -DSAFETY -DNOCOMP pan.c -o pan && ./pan -E -m1000000 $@
+# BITHASH
+spin -a $MODEL && gcc -DNOREDUCE -DSAFETY -DNOCOMP -DBITSTATE pan.c -o pan && ./pan -E -m1000000 -w33 $@
+rm pan*
--- src/pspin/spin-test.sh
+++ src/pspin/spin-test.sh
@@ -2,7 +2,8 @@
 
 MODEL=test.pml
 
-spin -a $MODEL
-gcc -DNOREDUCE -DSAFETY -DNOCOMP pan.c -o pan
-./pan -E -m100000
+# FULLSTATE
+spin -a $MODEL && gcc -DNOREDUCE -DSAFETY -DNOCOMP pan.c -o pan && ./pan -E -m1000000 $@
+# BITHASH
+#spin -a $MODEL && gcc -DNOREDUCE -DSAFETY -DNOCOMP -DBITSTATE pan.c -o pan && ./pan -E -m1000000 -w33 $@
 rm pan*
