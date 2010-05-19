b66f8a5801fa8832647eba09a69240181ef1700e
nkik <nkik@niisi.ras.ru>
2010-05-19 19:34:21 +0400

    Fixed scripts to work with multiple simultaneous jobs.

--- src/pspin/tools/process-results.sh
+++ src/pspin/tools/process-results.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+mv pspin.*stderr pspin.stderr
+mv pspin.*stdout pspin.stdout 
+
 for FILE in pspin.stdout pspin.stderr; do
 	[ -e $FILE ] && cat $FILE | sort -k1,1 -s | grep -v PROGRESS > $FILE.log
 	rm -f $FILE
 done
 
-cat pspin.stderr.log 2>/dev/null || echo "==VERIFICATION PASSED"
+cat pspin.stderr.log | grep -v indirect_max_size 2>/dev/null
--- src/pspin/tools/run-mpi-job.sh
+++ src/pspin/tools/run-mpi-job.sh
@@ -2,18 +2,22 @@
 
 JOB_NAME=pspin.job
 
-rm -f pspin.std*
+rm -f pspin.*std*
 
 make pspin -f Makefile.bare || exit 1
 
-llsubmit $JOB_NAME || exit 1
+SUBMIT=$(llsubmit $JOB_NAME)
+
+echo $SUBMIT
+JOBID=$(echo $SUBMIT | grep -o 'mgmt.nodes.[0-9]*')
+echo "Job ID: $JOBID"
 
 cleanup ()  {
 	llcancel -u stud057
 }
 trap cleanup HUP TERM INT ABRT QUIT 
 
-while ! llq -x | grep -q 'no job' ; do
-	llq -x
+while ! llq -x $JOBID | grep -q 'no job' ; do
+	llq -x $JOBID
 	sleep 5
 done
